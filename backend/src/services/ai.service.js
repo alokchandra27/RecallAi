b3ff20308fac219df2861c5402afe0748385817c
alok-404 <alokchandra2621@gmail.com>
2026-08-21 12:25:09 +0530

    Implemented short term memory so that AI model can remember what user is talking about

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -4,17 +4,33 @@ import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({});
 
-async function run() {
-  const interaction = await ai.interactions.create({
-    model: "gemini-3.6-flash",
-    input: "How does AI work?",
-  });
-  console.log(interaction.output_text);
+// async function run() {
+//   const interaction = await ai.interactions.create({
+//     model: "gemini-2.5-flash",
+//     input: "How does AI work?",
+//   });
+//   console.log(interaction.output_text);
+// }
+
+async function generateAIResponse(content) {
+  try {
+    const interaction = await ai.interactions.create({
+      model: "gemini-2.5-flash",
+      input: content,
+    });
+    return interaction.output_text;
+  }
+  catch (error) {
+    console.error("Error generating AI response:", error);
+    throw error;
+  }
 }
 
+export { generateAIResponse };
 
 
-run().catch((error) => {
-  console.error("Error:", error);
-})
+
+// run().catch((error) => {
+//   console.error("Error:", error);
+// })
 
--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -2,16 +2,18 @@ const { Server } = require("socket.io");
 const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
+const aiService = require("../services/ai.service");
+const messageModel = require("../models/message.model");
 
 function initializeSocketServer(httpServer) {
   const io = new Server(httpServer, {});
 
   io.use(async (socket, next) => {
     const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
-    // console.log("Socket handshake cookies:", cookies);
+    console.log("Socket handshake cookies:", cookies);
 
     if (!cookies.token) {
-    //   console.log("No cookies found in the handshake with socketio.");
+      //   console.log("No cookies found in the handshake with socketio.");
       return next(new Error("Authentication error: No cookies found"));
     }
 
@@ -22,17 +24,81 @@ function initializeSocketServer(httpServer) {
       socket.user = user;
       next();
     } catch (error) {
-    //   console.log("Error verifying JWT token:", error);
+      //   console.log("Error verifying JWT token:", error);
       return next(new Error("Authentication error: Invalid token"));
     }
   });
 
   io.on("connection", (socket) => {
-    console.log("Authenticated user:", socket.user);
-    console.log("New socket connection:", socket.id);
+    // console.log("Authenticated user:", socket.user);
+    // console.log("New socket connection:", socket.id);
 
-    socket.on("disconnect", () => {
-      console.log("A user disconnected:", socket.id);
+    // socket.on("disconnect", () => {
+    //   console.log("A user disconnected:", socket.id);
+    // });
+
+    socket.on("ai-message", async (messagePayload) => {
+      console.log("Received ai-message event:", messagePayload);
+
+      await messageModel.create({
+        user: socket.user._id,
+        chat: messagePayload.chat,
+        content: messagePayload.content,
+        role: "user_input",
+      });
+
+      const chatHistory = await messageModel.find({
+        chat: messagePayload.chat,
+      }).sort({ createdAt: -1 }).limit(10).lean().exec();
+
+      // console.log(
+      //   "chatHistory :",
+      //   chatHistory.map((item) => {
+      //     return {
+      //      type: item.role,
+      //      content: [
+      //        {
+      //           type: "text",
+      //           text: item.content,
+      //        },
+      //      ],
+      //     };
+      //   }),
+      // );
+
+      try {
+        const aiResponse = await aiService.generateAIResponse(
+          chatHistory.map((item) => {
+            return {
+              type: item.role,
+              content: [
+                {
+                  type: "text",
+                  text: item.content,
+                },
+              ],
+            };
+          }),
+        );
+        console.log("AI response generated:", aiResponse);
+
+        await messageModel.create({
+          user: socket.user._id,
+          chat: messagePayload.chat,
+          content: aiResponse,
+          role: "model_output",
+        });
+
+        socket.emit("ai-response", {
+          content: aiResponse,
+          chat: messagePayload.chat,
+        });
+      } catch (error) {
+        console.error("Error generating AI response:", error);
+        socket.emit("ai-response-error", {
+          error: "Failed to generate AI response",
+        });
+      }
     });
   });
 }
